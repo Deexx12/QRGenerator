66505b9ef84c8a8f6c9d4a43ff78aff53e4bb0ba
Deexx12 <156397282+Deexx12@users.noreply.github.com>
2024-02-01 22:20:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+
+import "./App.css";
+import Generator from "./QrGenerator/Generator";
+ 
+function App() {
+ return (
+  <>
+  <Generator></Generator>
+  </>
+  
+ )
+ }
+  
+  
+ 
+ 
+    
+       
+       
+
+   
+
+
+export default App;
